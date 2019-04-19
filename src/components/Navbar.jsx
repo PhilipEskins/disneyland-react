ed778df836f720af9bc4917fbb237ced4eadb406
Philip Eskins <philipeskins@gmail.com>
2019-04-19 10:02:03 -0700

    provide basic outline for navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,20 @@
 import React from 'react';
+import logo from '../assets/img/logo.png'
 
 function Navbar() {
   return(
     <div>
-      <h1>Navbar Works</h1>
+      Show Disney.com
+      Sign In or Create Account | United States(English) | Help
+      <input placeholder="search disneyland.com"></input>
+      <img src={logo} />
+      Parks & Tickets
+      Places to Stay
+      Things to do
+      Annual Passports
+      Shop
+      Cart
+      My Disneyland
     </div>
   );
 }
